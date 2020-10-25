d01e1db4c75ce8616a6b1288ed6ed922e498642d
doojin <dmitry.papka@gmail.com>
2020-10-25 17:31:39 -0500

    removing unused code

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByText('Hello, world!');
+  expect(divElement).toBeInTheDocument();
 });
