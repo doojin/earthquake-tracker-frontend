8077864f23a9a9ed36703337b2e0078b09cc01d0
doojin <dmitry.papka@gmail.com>
2020-10-25 21:38:30 -0500

    adding container div to markup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Container from 'react-bootstrap/Container'
+
+function App() {
+  return (
+    <Container>
+      Hello, world!!
+    </Container>
+  );
+}
+
+export default App;
