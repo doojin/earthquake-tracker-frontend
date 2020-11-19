4ad306ea8eaa8cbd22826e6c53c6e604d402b8fd
doojin <dmitry.papka@gmail.com>
2020-11-19 09:30:46 -0500

    different marker colors based on earthquake magnitude

--- src/components/EarthquakeMarker.js
+++ src/components/EarthquakeMarker.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Marker } from '@react-google-maps/api';
+import React, {useState} from 'react';
+import {Marker} from '@react-google-maps/api';
 import PropTypes from 'prop-types';
 import EarthquakePopup from './EarthquakePopup';
 
@@ -7,17 +7,27 @@ EarthquakeMarker.propTypes = {
   earthquake: PropTypes.object.isRequired,
 };
 
-export default function EarthquakeMarker({ earthquake }) {
-  const { latitude, longitude } = earthquake.position;
+const getMarkerColor = earthquake => {
+  if (earthquake.magnitude > 5) return 'red';
+  if (earthquake.magnitude > 3) return 'yellow';
+  return 'green';
+};
+
+export default function EarthquakeMarker({earthquake}) {
+  const {latitude, longitude} = earthquake.position;
   const [active, setActive] = useState(false);
 
+  const color = getMarkerColor(earthquake);
+  const icon = `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`;
+
   return (
-    <Marker position={{ lng: longitude, lat: latitude }}
+    <Marker icon={icon}
+            position={{lng: longitude, lat: latitude}}
             onClick={() => setActive(true)}>
 
-      <EarthquakePopup earthquake={ earthquake }
-                       active={ active }
-                       onClose={() => setActive(false)} />
+      <EarthquakePopup earthquake={earthquake}
+                       active={active}
+                       onClose={() => setActive(false)}/>
     </Marker>
   );
 };
--- src/components/EarthquakeMarker.test.js
+++ src/components/EarthquakeMarker.test.js
@@ -1,24 +1,76 @@
 import React from 'react';
 import EarthquakeMarker from './EarthquakeMarker';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 jest.mock('@react-google-maps/api', () => ({
   Marker: (props) => (
     <div>
       marker,
-      lng: { props.position.lng },
-      lat: { props.position.lat }
+      lng: {props.position.lng},
+      lat: {props.position.lat},
+      icon: {props.icon}
     </div>
   )
 }));
 
 describe('EarthquakeMarker', () => {
   test('renders Google Map marker', () => {
-    const earthquake = { position: { longitude: 1, latitude: 2 } };
-    render(<EarthquakeMarker earthquake={ earthquake } />);
+    const earthquake = {position: {longitude: 1, latitude: 2}};
+    render(<EarthquakeMarker earthquake={earthquake}/>);
 
     const marker = screen.queryByText(/marker/);
 
     expect(marker).toHaveTextContent('lng: 1, lat: 2');
+  });
+
+  describe('earthquake with magnitude > 5', () => {
+    test('renders green marker', () => {
+      const earthquake = {
+        magnitude: 6,
+        position: {
+          latitude: 1,
+          longitude: 2
+        }
+      };
+
+      render(<EarthquakeMarker earthquake={earthquake}/>);
+
+      expect(screen.queryByText(/marker/))
+        .toHaveTextContent('icon: http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    });
+  });
+
+  describe('earthquake with magnitude > 3 and < 5', () => {
+    test('renders yellow marker', () => {
+      const earthquake = {
+        magnitude: 4,
+        position: {
+          latitude: 1,
+          longitude: 2
+        }
+      };
+
+      render(<EarthquakeMarker earthquake={earthquake}/>);
+
+      expect(screen.queryByText(/marker/))
+        .toHaveTextContent('icon: http://maps.google.com/mapfiles/ms/icons/yellow-dot.png');
+    });
+  });
+
+  describe('earthquake with magnitude < 3', () => {
+    test('renders green marker', () => {
+      const earthquake = {
+        magnitude: 1,
+        position: {
+          latitude: 1,
+          longitude: 2
+        }
+      };
+
+      render(<EarthquakeMarker earthquake={earthquake}/>);
+
+      expect(screen.queryByText(/marker/))
+        .toHaveTextContent('icon: http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+    });
   });
 });
