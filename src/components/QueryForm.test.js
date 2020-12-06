630204bab965680b7112f39e55d94479e6d3ec75
doojin <dmitry.papka@gmail.com>
2020-12-06 09:27:33 -0300

    ET-1 adding unit tests for Limit field

--- src/components/QueryForm.test.js
+++ src/components/QueryForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import QueryForm from './QueryForm';
 import {Provider} from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
@@ -44,5 +44,84 @@ describe('query form', () => {
 
     expect(screen.queryByText('Depth (km):')).toBeInTheDocument();
     expect(screen.queryByRole('spinbutton', { name: 'Depth (km):' })).toBeInTheDocument();
+  });
+
+  describe('form validation', () => {
+    beforeEach(() => {
+      store = configureStore({
+        reducer: {
+          query: () => ({
+            limit: 100,
+            minMagnitude: 0,
+            maxMagnitude: 0,
+            startDateTime: Date.now(),
+            endDateTime: Date.now(),
+            minDepth: 0,
+            maxDepth: 0
+          })
+        }
+      });
+
+      render(
+        <Provider store={store}>
+          <QueryForm/>
+        </Provider>
+      );
+    });
+
+    describe('limit field', () => {
+      describe('entering not number', () => {
+        test('error is shown', async () => {
+          fireEvent.change(screen.queryByRole('spinbutton', { name: 'Limit:' }), {
+            target: {
+              value: 'notNumber'
+            }
+          });
+
+          await waitFor(() =>
+            expect(screen.queryByText('Limit should be a valid number')).toBeInTheDocument());
+        });
+      });
+
+      describe('value is less than minimal allowed', () => {
+        test('error is shown', async () => {
+          fireEvent.change(screen.queryByRole('spinbutton', { name: 'Limit:' }), {
+            target: {
+              value: '0'
+            }
+          });
+
+          await waitFor(() => expect(screen.queryByText('Minimal limit is 100')).toBeInTheDocument());
+        });
+      });
+
+      describe('value is greater than maximal allowed', () => {
+        test('error is shown', async () => {
+          fireEvent.change(screen.queryByRole('spinbutton', { name: 'Limit:' }), {
+            target: {
+              value: '1001'
+            }
+          });
+
+          await waitFor(() => expect(screen.queryByText('Maximal limit is 1000')).toBeInTheDocument());
+        });
+      });
+
+      describe('user enters a valid number', () => {
+        test('limit value is changed', async () => {
+          fireEvent.change(screen.queryByRole('spinbutton', { name: 'Limit:' }), {
+            target: {
+              value: '101'
+            }
+          });
+
+          fireEvent.blur(screen.queryByRole('spinbutton', { name: 'Limit:' }));
+
+          await waitFor(() =>
+            expect(screen.queryByRole('spinbutton', { name: 'Limit:' }))
+              .toHaveValue('101'));
+        });
+      });
+    });
   });
 });
