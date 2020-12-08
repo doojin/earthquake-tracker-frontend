6285ce0ed7cb21769c711358a6ff06d5404da28c
doojin <dmitry.papka@gmail.com>
2020-12-08 13:37:59 -0300

    ET-14 Google API key is secured now

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ import './App.less';
 
 // TODO: once query form is implemented: add unit tests
 function App() {
-  // TODO: verify what to do with this key
   const API_KEY = 'AIzaSyDMVX2ME7QpqJWf6hGmGoNY7wqTHJFO9wo';
 
   const dispatch = useDispatch();
