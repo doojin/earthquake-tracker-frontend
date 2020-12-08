93a3103484b92fb262e02f609bdba6e2af17eecd
doojin <dmitry.papka@gmail.com>
2020-12-08 19:26:38 -0300

    ET-17 translating relative date

--- src/components/RelativeDate.js
+++ src/components/RelativeDate.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import PropTypes from 'prop-types';
+import {enUS} from 'date-fns/locale';
+import {useTranslation} from 'react-i18next';
 
 RelativeDate.propTypes = {
   timestamp: PropTypes.number.isRequired
 };
 
 export default function RelativeDate({ timestamp }) {
+  const {t} = useTranslation('translation');
+  
   return (
     <span>
-      { formatDistanceToNow(timestamp) } ago
+      {
+        formatDistanceToNow(timestamp, {
+          locale: enUS
+        })
+      } {t('ago')}
     </span>
   );
 };
