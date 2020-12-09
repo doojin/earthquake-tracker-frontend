f949522bff290959d1f4665e2c414116d75422e3
doojin <dmitry.papka@gmail.com>
2020-12-08 23:01:58 -0300

    formatting relative date using correct locale

--- src/components/RelativeDate.test.js
+++ src/components/RelativeDate.test.js
@@ -2,14 +2,60 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { subDays } from 'date-fns';
 import RelativeDate from './RelativeDate';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import {enUS, ru} from 'date-fns/locale';
 
 describe('RelativeDate', () => {
-  test('formats date correctly', () => {
-    const timestamp = subDays(Date.now(), 2).getTime();
+  let store;
 
-    render(<RelativeDate timestamp={ timestamp } />);
+  describe('english locale', () => {
+    beforeEach(() => {
+      store = configureStore({
+        reducer: {
+          language: () => ({
+            locale: enUS
+          })
+        }
+      });
 
-    const formattedDate = screen.queryByText('2 days ago');
-    expect(formattedDate).toBeInTheDocument();
+      test('formats date correctly', () => {
+        const timestamp = subDays(Date.now(), 2).getTime();
+
+        render(
+          <Provider store={store}>
+            <RelativeDate timestamp={ timestamp } />
+          </Provider>
+        );
+
+        const formattedDate = screen.queryByText('2 days ago');
+        expect(formattedDate).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('russian locale', () => {
+    beforeEach(() => {
+      store = configureStore({
+        reducer: {
+          language: () => ({
+            locale: ru
+          })
+        }
+      });
+    });
+
+    test('formats date correctly', () => {
+      const timestamp = subDays(Date.now(), 2).getTime();
+
+      render(
+        <Provider store={store}>
+          <RelativeDate timestamp={ timestamp } />
+        </Provider>
+      );
+
+      const formattedDate = screen.queryByText('2 дня', {exact: false});
+      expect(formattedDate).toBeInTheDocument();
+    });
   });
 });
