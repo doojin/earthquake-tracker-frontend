290e49f9a5c4eb6cdc978712834b54db9129eea4
doojin <dmitry.papka@gmail.com>
2020-12-11 13:32:24 -0300

    implementing earthquakes multi view

--- src/components/EarthquakeView.js
+++ src/components/EarthquakeView.js
@@ -2,8 +2,22 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import EarthquakeMap from './EarthquakeMap';
 import {getAllEarthquakes} from '../store/slices/earthquakesSlice';
+import {Tabs} from 'antd';
+import {useTranslation} from 'react-i18next';
+import './EarthquakeView.less';
 
 export default function EarthquakeView() {
   const earthquakes = useSelector(getAllEarthquakes);
-  return <EarthquakeMap earthquakes={ earthquakes } center={{ latitude: 30, longitude: -85 }}/>;
+  const {t} = useTranslation('translation');
+
+  return (
+    <Tabs className="EarthquakeView">
+      <Tabs.TabPane tab={t('earthquakes.map')} className="EarthquakeMapTab" key="earthquakeMap">
+        <EarthquakeMap earthquakes={earthquakes} center={{latitude: 30, longitude: -85}}/>
+      </Tabs.TabPane>
+      <Tabs.TabPane tab={t('earthquakes.table')} key="earthquakeTable">
+        table
+      </Tabs.TabPane>
+    </Tabs>
+  );
 }
