a0daaee7f0f1138c14e5d31ce505e61a98226e33
doojin <dmitry.papka@gmail.com>
2020-12-11 17:17:51 -0300

    Earthquake API now supports location parameters

--- src/api/earthquakesApi.js
+++ src/api/earthquakesApi.js
@@ -13,7 +13,10 @@ export const getEarthquakes = async ({
   startDateTime,
   endDateTime,
   minDepth,
-  maxDepth
+  maxDepth,
+  latitude,
+  longitude,
+  radius
 }) => {
   const searchParams = new URLSearchParams();
   addSearchParam(searchParams, 'limit', limit);
@@ -23,11 +26,19 @@ export const getEarthquakes = async ({
   addSearchParam(searchParams, 'endTime', (endDateTime && moment(endDateTime).format()));
   addSearchParam(searchParams, 'minDepth', minDepth);
   addSearchParam(searchParams, 'maxDepth', maxDepth);
+  addSearchParam(searchParams, 'latitude', latitude);
+  addSearchParam(searchParams, 'longitude', longitude);
+  addSearchParam(searchParams, 'radius', radius);
 
   const search = searchParams.toString();
   const url = search ? `/earthquakes?${search}` : '/earthquakes';
 
   const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch earthquake data');
+  }
+
   const json = await response.json();
   return json.data;
 };
