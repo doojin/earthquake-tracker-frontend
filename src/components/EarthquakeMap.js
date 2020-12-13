b8396be55dcd06e33f5b119ff8a61e0b62dd8491
Dmitry Papka <dmitry.papka@gmail.com>
2020-12-13 15:00:43 -0300

    Displaying earthquakes area on map
    Displaying earthquakes area on map

--- src/components/EarthquakeMap.js
+++ src/components/EarthquakeMap.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { LoadScript, GoogleMap } from '@react-google-maps/api';
+import {LoadScript, GoogleMap} from '@react-google-maps/api';
 import EarthquakeMarker from './EarthquakeMarker';
 import './EarthquakeMap.less';
 import {removeActiveEarthquake} from '../store/slices/earthquakesSlice';
 import PropTypes from 'prop-types';
 import {useDispatch, useSelector} from 'react-redux';
 import {getLanguage} from '../store/slices/languageSlice';
+import EarthquakeArea from './EarthquakesArea';
 
 const API_KEY = 'AIzaSyDMVX2ME7QpqJWf6hGmGoNY7wqTHJFO9wo';
 
@@ -35,6 +36,7 @@ export default function EarthquakeMap({ earthquakes }) {
                  zoom={ 3 }
                  options={{ streetViewControl: false, mapTypeControl: false }}
                  onClick={() => dispatch(removeActiveEarthquake())}>
+        <EarthquakeArea />
         { markers }
       </GoogleMap>
     </LoadScript>
